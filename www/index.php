207024a5e6d8aea01efee125813748179240947c
Tarwin Stroh-Spijer <tarwin@gmail.com>
2013-03-03 18:18:25 -0800

    email address

--- www/index.php
+++ www/index.php
@@ -81,7 +81,7 @@
       <!-- FOOTER -->
       <footer>
         <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2013 SimpleWUIP, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        <p>&copy; 2013 SimpleWUIP, Inc. &middot; <a href="mailto:contact@simplewip.com">Contact</a></p>
       </footer>
 
     </div><!-- /.container -->
