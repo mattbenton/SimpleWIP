b94cf8ffe33364f0e322044635cbda085aaf7cf4
Tarwin Stroh-Spijer <tarwin@gmail.com>
2013-03-03 22:53:12 -0800

    invite as modal

---
+++ www/page/inc/stream_invite.php
@@ -0,0 +1,18 @@
+<!-- edit profile -->
+<div id="inviteModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="inviteModalLabel" aria-hidden="true">
+  <div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+		<h3 id="inviteModalLabel">Send Invitation</h3>
+  </div>
+
+  <div class="modal-body">
+	
+	<p>Please send the below link to your co-worker (<span id="invite-link-email"></span>) to get them to join your organisation.</p>
+	<div id="invite-link"></div>
+		
+  </div>
+
+  <div class="modal-footer">
+		<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+  </div>
+</div>
